220ce9d90f4296a32515b122f8f1212fb9475fc1
WASIMSKH <34674350+WASIMSKH@users.noreply.github.com>
2023-11-23 22:30:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
+
 app=Flask(__name__)
 # Load the model
 regmodel=pickle.load(open('regmodel.pkl','rb'))
